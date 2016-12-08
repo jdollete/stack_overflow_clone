5c4908fd953849aa0f238c6ec6a191c276586970
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2016-12-08 17:46:29 -0500

    Create migrations:  for all models

--- db/migrate/20161208223409_create_questions.rb
+++ db/migrate/20161208223409_create_questions.rb
@@ -1,8 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
+      t.string :title, null: false
+      t.string :body, null: false
+      t.references :user, null: false, index: true
+      t.references :answer, index: true #best answer
 
-      t.timestamps
+      t.timestamps(null: false)
     end
   end
 end
--- db/migrate/20161208223430_create_votes.rb
+++ db/migrate/20161208223430_create_votes.rb
@@ -1,8 +1,10 @@
 class CreateVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :votes do |t|
+      t.references :user, null: false
+      t.references :voteable, polymorphic: true, index: true
 
-      t.timestamps
+      t.timestamps(null: false)
     end
   end
 end
